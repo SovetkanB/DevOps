3926659aecc249288c0ba6bfb006bff911c0f679
Bakdaulet <bakdaulet.@gmail.com>
2022-12-23 12:11:20 +0600

    More about 'print' function

--- Python/hello.py
+++ Python/hello.py
@@ -1,3 +1,9 @@
-# This comment
+# This is comment
 my_hello = "Hello"
-print(my_hello)
+print(my_hello, 1)
+#Hello 1
+print( my_hello, 1, 2)
+#Hello 1 2
+print(my_hello, 1, 2, sep="", end=" ")
+print(my_hello, 1, 2, sep="")
+#Hello12 Hello12
